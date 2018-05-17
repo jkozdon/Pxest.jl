1ae87bdd078338356da856bf5e34646a648d8e71
Jeremy Edward Kozdon <jekozdon@nps.edu>
2018-05-16 21:34:27 -0700

    Adding p8est to the test

--- test.jl
+++ test.jl
@@ -5,12 +5,20 @@ end
 
 include("p4est.jl")
 using p4est
-
 let
   conn = p4est.Connectivity(5,7)
   pxest = p4est.PXEST(conn; min_lvl=2)
   p4est.balance(pxest)
   p4est.partition(pxest)
+end
+
+include("p8est.jl")
+using p8est
+let
+  conn = p8est.Connectivity(5,7,2)
+  pxest = p8est.PXEST(conn; min_lvl=1)
+  p8est.balance(pxest)
+  p8est.partition(pxest)
 end
 
 if !isinteractive()
