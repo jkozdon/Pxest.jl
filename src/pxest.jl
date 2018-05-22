6dad5ff8bf9c0caf9352f31f2eae2046cbfdc3f2
Jeremy Edward Kozdon <jekozdon@nps.edu>
2018-05-22 15:40:17 -0700

    moving around some files

---
+++ src/pxest.jl
@@ -0,0 +1,30 @@
+module pxest
+
+module p4est
+  macro p4est(ex)
+    return :($(esc(ex)))
+  end
+
+  macro p8est(ex)
+    return :()
+  end
+
+  include("pxest-base.jl")
+
+end
+
+module p8est
+
+  macro p4est(ex)
+    return :()
+  end
+
+  macro p8est(ex)
+    return :($(esc(ex)))
+  end
+
+  include("pxest-base.jl")
+
+end
+
+end
